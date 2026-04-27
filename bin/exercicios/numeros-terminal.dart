7738897f6584c105854dc91e6887516a2202e7c0
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-04-27 16:08:58 -0300

    adiciona exercício para imprimir números e nomes no terminal

---
+++ bin/exercicios/numeros-terminal.dart
@@ -0,0 +1,11 @@
+//1) Imprimindo números no terminal
+//Para imprimir os números de 1 a 5, podemos utilizar o seguinte código:
+
+void main() {
+  int limite = 5; // Defina o valor do limite
+
+  for (int i = 0; i < limite; i++) {
+    print('O valor de i é: $i');
+  }
+}
+
